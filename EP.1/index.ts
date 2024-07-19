3e77a5ad434735c27449eeedc7b2b04d215cb463
Anchalee-C <anchalee.salu@gmail.com>
2024-07-19 16:24:20 +0700

    no

--- EP.1/index.ts
+++ EP.1/index.ts
@@ -1,11 +1,11 @@
 const firstname: string = 'Anchalee'
 let y: number = 10
  y = 20
-let ต้องการกลับบ้าน: boolean = true
+let iWantToGo: boolean = true
 
 console.log(firstname)
 console.log(y)
-console.log(ต้องการกลับบ้าน)
+console.log(iWantToGo)
 
 const a: number = 2
 const b: number = 3
